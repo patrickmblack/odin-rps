66b93d2e413ccd21d6c0069997a6dc0b4bf54627
Patrick <50429647+patrickmblack@users.noreply.github.com>
2023-09-29 12:11:55 -0700

    Verify correct player input
    Player input is verified to be one of the options. If not, repeat
    until they enter a correct option. Computer and player choices
    are displayed in console properly.

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Hello World!")
+console.log(getPlayerChoice())
+console.log(getComputerChoice())
+
+
+
+
+function getComputerChoice(){
+
+    switch (Math.floor(Math.random() * 3)){
+        case 0:
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2:
+            return "Scissors"
+    }
+}
+
+function getPlayerChoice(){
+    //let playerInput = prompt("[Rock / Paper / Scissors]\n")
+    let validInput = false
+
+    while(!validInput){
+        switch(prompt("[Rock / Paper / Scissors]\n").slice(0, 1).toUpperCase()){
+            case "R":
+                validInput = true
+                return "Rock"
+            case "P":
+                validInput = true
+                return "Paper"
+            case "S":
+                validInput = true
+                return "Scissors"
+        }
+    }
+}
+
+function getWinner(computer, player){
+    
+}
+// for (let i = 0; i < 10; i++){
+//     console.log(getComputerChoice())
+// }
