9a443909d1d3503ac5847606ba569907931232fa
Patrick <50429647+patrickmblack@users.noreply.github.com>
2023-10-11 11:40:48 -0700

    Add buttons for player to make selection
    Buttons added and work to play a round of the game. Choices and outcome
    are printed in console.

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 //console.log(getWinner(getPlayerChoice(), getComputerChoice()))
-
 game()
 
-function game(){
-    let numOfGames = parseInt(prompt("How many games would you like to play?"))
 
-    for (let i = 0; i < numOfGames; i++){
-        playRound()
-    }
+
+function game(){
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            //console.log(button.id) 
+            playRound(button.id);
+        })
+    })
+
+    // let numOfGames = parseInt(prompt("How many games would you like to play?"))
+ 
+    // for (let i = 0; i < numOfGames; i++){
+    //     playRound()
+    // }
 }
 
-function playRound(){
-    let playerChoice = getPlayerChoice()
+function playRound(choice){
+
+
+    let playerChoice = choice
     let computerChoice = getComputerChoice()
     let winner = getWinner(playerChoice, computerChoice)
 
-    console.log(`${playerChoice} and ${computerChoice}, ${winner}`)
+    console.log(`Player chooses ${playerChoice}\nComputer chooses ${computerChoice}\n${winner}`)
 }
 
 function getComputerChoice(){
@@ -30,24 +42,29 @@ function getComputerChoice(){
     }
 }
 
-function getPlayerChoice(){
-    //let playerInput = prompt("[Rock / Paper / Scissors]\n")
-    let validInput = false
 
-    while(!validInput){
-        switch(prompt("[Rock / Paper / Scissors]\n").slice(0, 1).toUpperCase()){
-            case "R":
-                validInput = true
-                return "Rock"
-            case "P":
-                validInput = true
-                return "Paper"
-            case "S":
-                validInput = true
-                return "Scissors"
-        }
-    }
-}
+
+// function getPlayerChoice(){
+//     //let playerInput = prompt("[Rock / Paper / Scissors]\n")
+//     let validInput = false
+
+
+
+
+//     while(!validInput){
+//         switch(prompt("[Rock / Paper / Scissors]\n").slice(0, 1).toUpperCase()){
+//             case "R":
+//                 validInput = true
+//                 return "Rock"
+//             case "P":
+//                 validInput = true
+//                 return "Paper"
+//             case "S":
+//                 validInput = true
+//                 return "Scissors"
+//         }
+//     }
+// }
 
 function getWinner(player, computer){
     if (player === computer){
